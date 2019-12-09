086f0b98369632459527e5b835e34d45e76662be
AaronMullan <aaroncmullan@gmail.com>
2019-12-08 22:47:36 -0800

    page 41

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
-var exec =  require('child_process').exec;
+const querystring = require('querystring'), fs =require('fs');
 
-function start(response) {
+
+function start(response, postData) {
     console.log('Request handler "start" was called.');
 
     const body = '<html>' +
@@ -21,13 +22,20 @@ function start(response) {
         response.end();
 }
 
-
-function upload(response) {
+function upload(response, postData) {
     console.log('Request handler "upload" was called.');
-    response.writeHead(200, {'Content-Type': "text/plain"});
-    response.write('Hello Upload');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('You\'ve sent the text:' + 
+    querystring.parse(postData).text);
     response.end();
+}
+
+function show (response) {
+    console.log('Request handler "show" was called.');
+    response.writeHead(200, {'Content-Type': 'img/png'});
+    fs.createReadStream('tmp/test/png').pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
